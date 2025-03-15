3346b1cb75d6c185f6e23eb5f8f925c41c885939
remi taunay <remi.taunay@gmail.com>
2025-03-15 18:50:12 +0100

    wip js load mmd from file

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
 
-mermaid.initialize({ startOnLoad: true, theme: "default" });
+async function getMMD() {
+  try {
+    // from file
+    const response = await fetch("game.elk.mmd");
+    if (!response.ok) throw new Error(`error http ${response.status} https://http.cat/${response.status}`);
+  } catch (error) {
+    console.error("Error loading Mermaid diagram:", error);
+    return "Failed to load diagram."
+  }
+  return TODOOOO
+}
+
+
+async function main() {
+
+  // from file get mmd
+  const response = await fetch(filePath);
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+  const mmd = await response.text();
+  document.getElementById("diagram-container").textContent = mmd;
+
+  // render
+  mermaid.initialize({
+    startOnLoad: false,
+    securityLevel: 'loose',
+  });
+  await mermaid.run({
+    querySelector: '.mermaid',
+  });
+}
+
+main();
+
+document.getElementById("diagram-container").textContent = "Failed to load diagram.";
